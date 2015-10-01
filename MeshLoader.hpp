7c0691cce2a269389ca836675648ea2e0839d584
Harry Jeffery <harry@exec64.co.uk>
2015-10-01 09:36:12 +0100

    Add .obj mesh loader

--- MeshLoader.hpp
+++ MeshLoader.hpp
@@ -2,11 +2,14 @@
 
 #include "Mesh.hpp"
 
+#include <string>
+
 namespace he
 {
   class MeshLoader
   {
   public:
+    static Mesh* LoadOBJ(const std::string &path);
     static Mesh* Dummy();
   };
 }
