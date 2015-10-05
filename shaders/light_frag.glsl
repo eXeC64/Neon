f6309851e87163f4e797944b89fac00526ecd308
Harry Jeffery <harry@exec64.co.uk>
2015-10-05 16:09:59 +0100

    Add basic hard-coded lighting

--- shaders/light_frag.glsl
+++ shaders/light_frag.glsl
@@ -9,8 +9,11 @@ uniform sampler2D sampNormal;
 uniform sampler2D sampDepth;
 
 uniform float time;
+uniform vec3 viewPos;
 uniform vec2 screenSize;
 uniform mat4 matView;
+
+mat4 invMatView = inverse(matView);
 
 vec2 screenPos = gl_FragCoord.xy / screenSize;
 
@@ -21,18 +24,28 @@ float normalizeDepth(float value, float min, float max)
   return invFar / (Q - value);
 }
 
-vec3 worldPos()
+vec3 calcWorldPos()
 {
  float z = texture(sampDepth, screenPos).x;
  vec4 sPos = vec4(screenPos * 2.0 - 1.0, z * 2.0 - 1.0, 1.0);
- sPos = inverse(matView) * sPos;
+ sPos = invMatView * sPos;
  return sPos.xyz / sPos.w;
 }
 
 void main()
 {
   vec3 diffuse = texture(sampDiffuse, screenPos).rgb;
-  vec3 normal = texture(sampNormal, screenPos).rgb;
+  vec3 worldNormal = normalize(texture(sampNormal, screenPos).xyz);
+  vec3 worldPos = calcWorldPos();
   float depth = texture(sampDepth, screenPos).x;
-  outColor = diffuse;
+
+  vec3 lightPos = 3.0 * vec3(sin(2.0*time),1.0,cos(2.0*time));
+  vec3 lightDir = normalize(lightPos - worldPos);
+  float light = clamp(dot(lightDir,worldNormal), 0.15, 1.0);
+
+  outColor = vec3(0.5);
+  if(depth < 1.0)
+  {
+    outColor = light * diffuse;
+  }
 }
