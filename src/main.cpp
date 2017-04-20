e87b7e47881cb370f5c780d59c94e0bffce489ab
Harry Jeffery <harry@exec64.co.uk>
2017-04-20 23:06:07 +0100

    Improve debugging support

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,59 @@
 #include "OpenGL.hpp"
 #include "Renderer.hpp"
 #include "Loader.hpp"
+#include "Model.hpp"
 #include "Mesh.hpp"
 #include "Texture.hpp"
 #include "Material.hpp"
+
+void APIENTRY glDebugOutput(GLenum source,
+                            GLenum type,
+                            GLuint id,
+                            GLenum severity,
+                            GLsizei length,
+                            const GLchar *message,
+                            const void *userParam)
+{
+  // ignore non-significant error/warning codes
+  if(id == 131204 || id == 131185)
+    return;
+  /* if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return; */
+
+  std::cout << "---------------" << std::endl;
+  std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+  switch (source)
+  {
+    case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+  } std::cout << std::endl;
+
+  switch (type)
+  {
+    case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+  } std::cout << std::endl;
+
+  switch (severity)
+  {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+  } std::cout << std::endl;
+  std::cout << std::endl;
+}
 
 int main(int argc, char **argv)
 {
@@ -36,13 +86,13 @@ int main(int argc, char **argv)
 
   SDL_GLContext GLcontext = SDL_GL_CreateContext(pWindow);
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+  glEnable(GL_DEBUG_OUTPUT);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageCallback(&glDebugOutput, nullptr);
+  glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
   std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
-  /* std::cout << "Extensions: " << std::endl; */
-  /* GLint numExts; */
-  /* glGetIntegerv(GL_NUM_EXTENSIONS, &numExts); */
-  /* for(int i = 0; i < numExts; ++i) */
-  /*   std::cout << " * " << glGetStringi(GL_EXTENSIONS, i) << "\n"; */
-  /* std::cout << std::flush; */
 
   he::Renderer* pRenderer = new he::Renderer();
 
