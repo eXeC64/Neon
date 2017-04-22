9a90451c73e9ebc69bfb5afa0436f5d44763550c
Harry Jeffery <harry@exec64.co.uk>
2017-04-22 15:31:24 +0100

    Gamma correct lighting

--- shaders/light_frag.glsl
+++ shaders/light_frag.glsl
@@ -15,9 +15,10 @@ uniform vec3 lightColor;
 uniform vec2 screenSize;
 uniform mat4 matView;
 
-mat4 invMatView;
-
-vec2 screenPos;
+vec3 gammaCorrect(float gamma, vec3 color)
+{
+  return pow(color, vec3(1.0/gamma));
+}
 
 float normalizeDepth(float value, float min, float max)
 {
@@ -26,31 +27,32 @@ float normalizeDepth(float value, float min, float max)
   return invFar / (Q - value);
 }
 
-vec3 calcWorldPos()
+vec3 calcWorldPos(vec2 screenPos)
 {
  float z = texture(sampDepth, screenPos).x;
  vec4 sPos = vec4(screenPos * 2.0 - 1.0, z * 2.0 - 1.0, 1.0);
+ mat4 invMatView = inverse(matView);
  sPos = invMatView * sPos;
  return sPos.xyz / sPos.w;
 }
 
 void main()
 {
-  screenPos = gl_FragCoord.xy / screenSize;
-  invMatView = inverse(matView);
+  vec2 screenPos = gl_FragCoord.xy / screenSize;
   vec3 diffuse = texture(sampDiffuse, screenPos).rgb;
   vec3 worldNormal = normalize(texture(sampNormal, screenPos).xyz);
-  vec3 worldPos = calcWorldPos();
+  vec3 worldPos = calcWorldPos(screenPos);
   float depth = texture(sampDepth, screenPos).x;
 
   vec3 lightDir = normalize(worldPos - lightPos);
   float lambert = clamp(dot(lightDir,worldNormal), 0.15, 1.0);
   float lightDist = distance(lightPos, worldPos);
-  float light = lambert * (10.0 / lightDist);
+  float attenuation = 1.0 / (lightDist * lightDist);
+  float light = 5.0 * lambert * attenuation;
 
   outColor = vec3(0.5);
   if(depth < 1.0)
   {
-    outColor = lightColor * light * diffuse;
+    outColor = gammaCorrect(2.2, lightColor * light * diffuse);
   }
 }
