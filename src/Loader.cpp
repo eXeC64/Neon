6c52aed2cb920700d0f6d10ac7b8312860fda56e
Harry Jeffery <harry@exec64.co.uk>
2017-05-02 21:54:29 +0100

    Mipmapping

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -218,9 +218,10 @@ namespace ne
     glGenTextures(1, &pTex->m_glTexture);
     glBindTexture(GL_TEXTURE_2D, pTex->m_glTexture);
     glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, &data[0]);
+    glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glBindTexture(GL_TEXTURE_2D, 0);
 
