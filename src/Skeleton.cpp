05128d1644b871d2bc17cef375873d6602e5105a
Harry Jeffery <harry@exec64.co.uk>
2017-05-12 23:20:46 +0100

    Clean up Skeleton.cpp

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -7,10 +7,7 @@ namespace ne
   {
     //Calculate local transformation matrix for each bone first
     for(size_t i = 0; i < bones.size(); ++i)
-    {
-      /* outTrans[i] = glm::mat4_cast(bones[i].localRot) * glm::translate(bones[i].localPos); */
       outTrans[i] = glm::translate(bones[i].localPos) * glm::mat4_cast(bones[i].localRot);
-    }
 
     //Chain the transforms down the skeleton
     chainTransforms(glm::mat4(1.0), 0, outTrans);
